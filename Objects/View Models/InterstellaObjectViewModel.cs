4fd54c996797a385a9bb75cdfcb2b77390f84f67
Jack Walton <jackwal99@gmx.com>
2018-07-29 11:48:14 +0100

    Added Testing Console and Mass Constant

--- Objects/View Models/InterstellaObjectViewModel.cs
+++ Objects/View Models/InterstellaObjectViewModel.cs
@@ -26,8 +26,8 @@ namespace OrbitalSimulator_Objects
             _Position = interstellaObject.Position;
 
             // ## Set a radius inside IntersetellaObject which is based off of mass
-            _Width = 50;
-            _Height = 50;
+            _Width = interstellaObject.Radius * 2;
+            _Height = interstellaObject.Radius * 2;
         }
 
         public Vector Position
