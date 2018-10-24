a3d5393c2422f5f0ccd69287cdc1645325b33867
Jack Walton <jackwal99@gmx.com>
2018-10-24 17:24:01 +0100

    Revert "Merge branch 'Beta'"
    This reverts commit 1f7802600c3d9bb6bd662842e014e2326aac9c26, reversing
    changes made to 39360f2b8c1535349b33155df645d81417e6b090.

---
+++ Objects/View Models/InterstellaSystemViewModel.cs
@@ -0,0 +1,55 @@
+﻿using OrbitalSimulator_Objects;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace OrbitalSimulator_Objects
+{
+    #region !!! Obsolte !!! InterstellaSystemViewModel
+
+    // ## Do i really need this class, It is a view model that will never by bound too, this functionality could just be handled in the CanvasPage View Model
+    //public class InterstellaSystemViewModel : BaseViewModel
+    //{
+    //    private InterstellaSystem _System;
+    //    private ObservableCollection<InterstellaObjectViewModel> _ObjectVMs;
+
+    //    public InterstellaSystemViewModel(InterstellaSystem system)
+    //    {
+    //        _System = system;
+
+    //        // Project System InterstellaObejct's Into thier VMs
+    //        ObjectVMs = new ObservableCollection<InterstellaObjectViewModel>(
+    //            _System.InterstellaObjects.Select(
+    //                InterstellaObject => new InterstellaObjectViewModel(InterstellaObject)
+    //            ));
+    //    }
+
+    //    public ObservableCollection<InterstellaObjectViewModel> ObjectVMs
+    //    {
+    //        get => _ObjectVMs;
+    //        set
+    //        {
+    //            _ObjectVMs = value;
+    //            NotifyPropertyChanged(this , nameof(ObjectVMs));
+    //        }
+    //    }
+
+    //    public void StartSystem()
+    //    {
+    //        _System.Start();
+    //    }
+
+    //    public void StopSystem()
+    //    {
+    //        _System.Stop();
+    //    }
+    //}
+
+    #endregion
+}
